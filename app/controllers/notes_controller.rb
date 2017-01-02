a909a5c15bbe6f754ad102b099c242fdb85a0b73
vagrant <vagrant@localhost.localdomain>
2017-01-02 18:49:14 +0900

    update complete

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -6,14 +6,26 @@ class NotesController < ApplicationController
     @note = Note.new
     @note.content = params[:content]
     @note.save
-    redirect_to action: 'show'
+    redirect_to note_path(@note.id)
   end
 
   def show
-    @note = Note.all
+    @note = Note.find(params[:id])
   end
 
   def index
     @notes = Note.all
+  end
+
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+  def update
+    @note = Note.find(params[:id])
+    @note.content = params[:content]
+    @note.save
+
+    redirect_to note_path(@note.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
 
-  root 'notes#new'
+  root 'notes#index'
 
   get '/new' => 'notes#new'
 
   post 'notes' => 'notes#create'
 
-  get 'note' => 'notes#show'
+  get 'note/:id' => 'notes#show' ,as:'note'
 
   get 'notes' => 'notes#index'
+
+  get 'edit/:id' => 'notes#edit',as:'edit'
+
+  patch 'notes/:id' => 'notes#update',as:'update_note'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
