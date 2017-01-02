281c4c3d4cb4d438701c63ce5fd0c7b078b4458a
vagrant <vagrant@localhost.localdomain>
2017-01-02 17:20:27 +0900

    index

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -3,5 +3,17 @@ class NotesController < ApplicationController
   end
 
   def create
+    @note = Note.new
+    @note.content = params[:content]
+    @note.save
+    redirect_to action: 'show'
+  end
+
+  def show
+    @note = Note.all
+  end
+
+  def index
+    @notes = Note.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
 
-  get 'new' => 'notes#new'
+  root 'notes#new'
+
+  get '/new' => 'notes#new'
 
   post 'notes' => 'notes#create'
+
+  get 'note' => 'notes#show'
+
+  get 'notes' => 'notes#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
